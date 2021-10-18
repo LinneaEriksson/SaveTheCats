171a7b0f1103fda4dce4d8d3996a33a84a4b7837
LinneaEriksson <Linnea-E@hotmail.com>
2021-10-18 14:19:53 +0200

    Added pictures to the cats

--- index.php
+++ index.php
@@ -1,28 +1,33 @@
 <?php
 
 require __DIR__ . '/header.html';
+require __DIR__ . '/index.html';
 
 
 $cats = [
   [
+    'img' => '/Pictures/Cat3.jpg',
     'Namn' => 'Tasse',
     'Ras' => 'Huskatt',
     'Ålder' => '10år',
     'Information' => 'Tasse är en skygg katt som vill komma till ett hem utan småbarn. Tasse behöver mycket eget utrymme och vill umgås på egna villkor'
   ],
   [
+    'img' => '/Pictures/Cat4.jpg',
     'Namn' => 'Nasse',
     'Ras' => 'Huskatt',
     'Ålder' => '8år',
     'Information' => 'Nasse är en världens gosigaste katt som vill komma till ett hem med mycket kärlek. Nasse vill vara en innekatt i sitt nya hem.'
   ],
   [
+    'img' => '/Pictures/Cat5.jpg',
     'Namn' => 'Riffe',
-    'Ras' => 'Sibirisk katt',
+    'Ras' => 'Sibirisk katt/Main Coon',
     'Ålder' => '1år',
     'Information' => 'Här har vi ett riktigt busfrö. Riffe vill bo i ett hem med vuxna ägare då han blir stressad utav barn. Riffe får gärna vara ute när han blir äldre.'
   ],
   [
+    'img' => '/Pictures/Cat6.jpg',
     'Namn' => 'Chewie',
     'Ras' => 'Huskatt',
     'Ålder' => '2år',
@@ -31,15 +36,32 @@ $cats = [
 ];
 
 foreach ($cats as $cat) :
+  $image = $cat['img'];
   $name = $cat['Namn'];
   $rase = $cat['Ras'];
   $age = $cat['Ålder'];
   $informations = $cat['Information'];
 
+?>
+  <div class="information">
+    <div class="GridInformation">
 
-  echo "$name $rase $age $informations";
+      <br>
+      <ul>
+        <li><img src=" <?php echo "$image";
+                        ?>"></li>
+        <li> <?php echo "Namn: $name";
+              ?></li>
+        <li><?php echo "Ras: $rase";
+            ?></li>
+        <li><?php echo "Ålder: $age";
+            ?></li>
+        <li><?php echo "Information: $informations";
+            ?></li>
+      </ul></br>
+    </div>
+  </div><?php
+      endforeach;
 
-endforeach;
 
-
-require __DIR__ . '/footer.html';
+      require __DIR__ . '/footer.html';
