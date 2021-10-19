bba75f2e00ba42b160c8b4fe1182d84baeb4d32b
LinneaEriksson <Linnea-E@hotmail.com>
2021-10-19 15:36:11 +0200

    Fixed bugs in css

---
+++ arrays.php
@@ -0,0 +1,32 @@
+<?php
+
+$cats = [
+  [
+    'img' => '/Pictures/Cat3.jpg',
+    'Namn' => 'Tasse',
+    'Ras' => 'Huskatt',
+    'Ålder' => '10år',
+    'Information' => 'Tasse är en tuff katt som vill komma till ett hem utan småbarn. Tasse behöver mycket eget utrymme och vill umgås på egna villkor'
+  ],
+  [
+    'img' => '/Pictures/Cat4.jpg',
+    'Namn' => 'Nasse',
+    'Ras' => 'Huskatt',
+    'Ålder' => '8år',
+    'Information' => 'Nasse är en världens gosigaste katt som vill komma till ett hem med mycket kärlek. Nasse vill vara en innekatt i sitt nya hem och behöver hamna i ett hem utan andra katter.'
+  ],
+  [
+    'img' => '/Pictures/Cat5.jpg',
+    'Namn' => 'Riffe',
+    'Ras' => 'Sibirisk katt/Main Coon',
+    'Ålder' => '1år',
+    'Information' => 'Här har vi ett riktigt busfrö. Riffe vill bo i ett hem med vuxna ägare då han blir stressad utav barn. Riffe får gärna vara ute när han blir äldre.'
+  ],
+  [
+    'img' => '/Pictures/Cat6.jpg',
+    'Namn' => 'Chewie',
+    'Ras' => 'Huskatt',
+    'Ålder' => '2år',
+    'Information' => 'Chewie har haft en tuff start på livet. Han är världens snällaste och vill komma till ett hem där han får stå i centrum för att våga ta plats.'
+  ]
+];
--- index.php
+++ index.php
@@ -1,67 +1,51 @@
 <?php
 
 require __DIR__ . '/header.html';
-require __DIR__ . '/index.html';
-
-
-$cats = [
-  [
-    'img' => '/Pictures/Cat3.jpg',
-    'Namn' => 'Tasse',
-    'Ras' => 'Huskatt',
-    'Ålder' => '10år',
-    'Information' => 'Tasse är en skygg katt som vill komma till ett hem utan småbarn. Tasse behöver mycket eget utrymme och vill umgås på egna villkor'
-  ],
-  [
-    'img' => '/Pictures/Cat4.jpg',
-    'Namn' => 'Nasse',
-    'Ras' => 'Huskatt',
-    'Ålder' => '8år',
-    'Information' => 'Nasse är en världens gosigaste katt som vill komma till ett hem med mycket kärlek. Nasse vill vara en innekatt i sitt nya hem.'
-  ],
-  [
-    'img' => '/Pictures/Cat5.jpg',
-    'Namn' => 'Riffe',
-    'Ras' => 'Sibirisk katt/Main Coon',
-    'Ålder' => '1år',
-    'Information' => 'Här har vi ett riktigt busfrö. Riffe vill bo i ett hem med vuxna ägare då han blir stressad utav barn. Riffe får gärna vara ute när han blir äldre.'
-  ],
-  [
-    'img' => '/Pictures/Cat6.jpg',
-    'Namn' => 'Chewie',
-    'Ras' => 'Huskatt',
-    'Ålder' => '2år',
-    'Information' => 'Chewie har haft en tuff start på livet. Han är världens snällaste och vill komma til ett hem där han får stå i centrum för att våga ta plats.'
-  ]
-];
-
-foreach ($cats as $cat) :
-  $image = $cat['img'];
-  $name = $cat['Namn'];
-  $rase = $cat['Ras'];
-  $age = $cat['Ålder'];
-  $informations = $cat['Information'];
+require __DIR__ . '/arrays.php';
 
 ?>
-  <div class="information">
-    <div class="GridInformation">
 
-      <br>
-      <ul>
-        <li><img src=" <?php echo "$image";
-                        ?>"></li>
-        <li> <?php echo "Namn: $name";
-              ?></li>
-        <li><?php echo "Ras: $rase";
-            ?></li>
-        <li><?php echo "Ålder: $age";
-            ?></li>
-        <li><?php echo "Information: $informations";
-            ?></li>
-      </ul></br>
+<body>
+
+  <div class="container">
+    <div class="content">
+      <img class="HeadPicture" src="/Pictures/Cat1.jpg">
+      <h1>Hemlösa katter</h1>
+
+
+
+      <?php foreach ($cats as $cat) :
+        $image = $cat['img'];
+        $name = $cat['Namn'];
+        $rase = $cat['Ras'];
+        $age = $cat['Ålder'];
+        $informations = $cat['Information'];
+
+      ?>
+        <p class="information">
+
+
+          <img src=" <?php echo "$image";
+                      ?>">
+          <?php echo "Namn: $name";
+          ?><br>
+          <?php echo "Ras: $rase";
+          ?><br>
+          <?php echo "Ålder: $age";
+          ?><br>
+          <?php echo "Information: $informations";
+          ?><br>
+
+
+        </p><?php
+          endforeach; ?>
+
     </div>
-  </div><?php
-      endforeach;
+  </div>
+</body>
+<?php
+$date = date("d-y-m");
 
+echo $date;
 
-      require __DIR__ . '/footer.html';
+require __DIR__ . '/footer.html';
